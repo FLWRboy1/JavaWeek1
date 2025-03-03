6af67af9b7ab3cfe71ed76c577ab7ba98d2f408d
FLWRboy1 <frankmullahy@gmail.com>
2025-03-03 10:07:55 -0500

    a

--- Comments.java
+++ Comments.java
@@ -1,8 +1,11 @@
-public class Comments {
+//Frank Mullahy
+//2/13/2025
 
-    public static void main (String[]args)
+public class NameScore {
+
+    public static void main (string[]args)
     {
-    System.out.println("Hi"); //message to user
-    System.out.println("LabOneA"); //assignment
+        System.out.println("\t\t\tname\tscore\n\t\t\tPicard\t1874\n\t\t\tRiker\t1789");
+
     }
 }
---
+++ Name.java
@@ -0,0 +1,14 @@
+//Frank Mullahy
+//2/13/2025
+
+
+public class Name {
+
+    public static void main (String[]args)
+    {
+        System.out.println("\t\t\tname\tscore\n\t\t\tPicard\t1874\n\t\t\tRiker\t1789");
+
+        System.out.println("\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t*\n\t\t\t\t\t*\n\t\t\t\t\t*\n");
+
+    }
+}
